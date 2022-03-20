d9c3ba64170b4961c8894d2caa294e5d18cccd4b
Nguyen Quy Minh <incarnation1602@gmail.com>
2022-03-20 18:56:36 +0700

    Active Swap

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+
+
 /**
  * Base
  */
@@ -64,6 +66,7 @@ controls.enableDamping = true
 /**
  * Set up parameters
  */
+const mouse = new THREE.Vector2(1, 1);
 let tiles = []
 let board = []
 
@@ -241,22 +244,24 @@ function swap(i, j, arr) {
 }
 
 
-// function randomMove(array){
-//     let j = Math.floor(Math.random()*array.length);
-//     // console.log(j)
-//         // const temp = array[i];
-//         // array[i] = array[j];
-//         // array[j] = temp;
-//         // swapcubeprop(array[i], array[j])
-//         // swap(i, j, array)
-//         // let r1 =    array[j].position.x
-//         // let r2 = array[j].position.z
+function randomMove(array) {
+    let j = Math.floor(Math.random() * array.length);
+    // console.log(j)
+    // const temp = array[i];
+    // array[i] = array[j];
+    // array[j] = temp;
+    // swapcubeprop(array[i], array[j])
+    // swap(i, j, array)
+    // let r1 =    array[j].position.x
+    // let r2 = array[j].position.z
 
-//         let r1 = Math.floor(Math.random(cols));
-//         let r2 = Math.floor(Math.random(rows));
-//     move(r1 , r2, array)
+    let r1 = Math.floor(Math.random(cols));
+    let r2 = Math.floor(Math.random(rows));
+    move(r1, r2, array)
 
-// }
+}
+// randomMove(tiles)
+
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -266,7 +271,7 @@ function shuffleArray(array) {
         // array[j] = temp;
         // swapcubeprop(array[i], array[j])
         swap(i, j, array)
-        // move(i , j , array)
+        // move(i, j, array)
     }
 }
 
@@ -282,29 +287,32 @@ shuffleArray(tiles)
 /**
  * Function
  */
-function move(i, j, arr) {
-    let blank = findBlank();
-    console.log(blank)
-    // let blankCol = blank % cols;
-    // let blankRow = Math.floor(blank / rows);
-    let blankCol = arr[blank].position.x
-    let blankRow = arr[blank].position.z
-    if (isNeighbor(i, j, blankCol, blankRow)) {
-        swap(blank, (i + j * cols), arr);
-    }
-}
+// function move(i, j, arr) {
+//     let blank = findBlank();
+//     console.log(blank)
+//     // let blankCol = blank % cols;
+//     // let blankRow = Math.floor(blank / rows);
+//     let blankCol = arr[blank].position.x
+//     let blankRow = arr[blank].position.z
 
+//     let chosenPos = i + j* cols
+//     if (isNeighbor(i, j, blankCol, blankRow)) {
+//         swap(blank, chosenPos, arr);
+//     }
+//     // swap(blank, (i + j * cols), arr);
+//     // swap(blank, chosenPos, arr);
+// }
 
-function isNeighbor(i, j, x, y) {
-    if (i !== x && j !== y) {
-        return false;
-    }
-    if (Math.abs(i - x) == 5 || Math.abs(j - y) == 5) {
-        return true;
-    }
+// function isNeighbor(i, j, x, y) {
+//     if (i !== x && j !== y) {
+//         return false;
+//     }
+//     if (Math.abs(i - x) == 5 || Math.abs(j - y) == 5) {
+//         return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
 
 function findBlank() {
@@ -324,10 +332,6 @@ tiles.forEach((tile) => {
     scene.add(tile);
 });
 
-
-
-
-
 /**
  * Renderer
  */
@@ -344,6 +348,107 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 let lastElapsedTime = 0
 
+
+
+class PickHelper {
+    constructor() {
+        this.raycaster = new THREE.Raycaster();
+        this.pickedObject = null;
+        this.pickedObjectSavedColor = 0;
+        this.blank = null;
+    }
+    pick(normalizedPosition, scene, camera) {
+        // restore the color if there is a picked object
+          if (this.pickedObject) {
+            // this.pickedObject.material.emissive.setHex(this.pickedObjectSavedColor);
+            this.pickedObject = undefined;
+          }
+
+
+
+        // cast a ray through the frustum
+        this.raycaster.setFromCamera(normalizedPosition, camera);
+        // get the list of objects the ray intersected
+        const intersectedObjects = this.raycaster.intersectObjects(scene.children);
+        if (intersectedObjects.length) {
+            // pick the first object. It's the closest one
+            this.pickedObject = intersectedObjects[0].object;
+            // save its color
+            // this.pickedObjectSavedColor = this.pickedObject.material.emissive.getHex();
+            // // set its emissive color to flashing red/yellow
+            // this.pickedObject.material.emissive.setHex((time * 8) % 2 > 1 ? 0xFFFF00 : 0xFF0000);
+
+            tiles.forEach(cube => {
+                if (cube.name == "15") {
+                    this.blank = cube
+                }
+            });
+
+            swap2cube(this.pickedObject, this.blank, tiles)
+
+            function swap2cube(pickedobject, blank, array) {
+                if (isNeighbor(pickedobject, blank)) {
+                    let temp1 = pickedobject.position.x
+                    pickedobject.position.x = blank.position.x
+                    blank.position.x = temp1
+
+                    let temp2 = pickedobject.position.z
+                    pickedobject.position.z = blank.position.z
+                    blank.position.z = temp2
+
+                    for (let i = 0; i < array.length; i++) {
+                        if (array[i].name == pickedobject.name) {
+
+                            for (let j = 0; j < array.length; j++) {
+                                if (array[j].name == blank.name) {
+                                    let temp = array[i]
+                                    array[i] = array[j]
+                                    array[j] = temp
+                                }
+                            }
+                        }
+                    }
+
+
+                    // cubes.forEach(cube1 => {
+                    //   if(cube1.id == pickedobject.id){
+
+                    //     cubes.forEach(cube2 => {
+                    //       if(cube2.id == blank.id){
+                    //         let temp = cube1
+                    //         cube1 = cube2
+                    //         cube2 = temp
+                    //       }
+                    //     });
+                    //   }
+                    // });
+                }
+
+            }
+
+            function isNeighbor(pickedobject, blank) {
+                if (pickedobject.position.x !== blank.position.x && pickedobject.position.z !== blank.position.z) {
+                    return false;
+                }
+                if (Math.abs(pickedobject.position.x - blank.position.x) == 5 || Math.abs(pickedobject.position.z - blank.position.z) == 5) {
+                    return true;
+                }
+
+                return false;
+            }
+            console.log(this.pickedObject.position.x, this.pickedObject.position.z)
+            console.log(tiles)
+
+        }
+    }
+}
+
+const pickPosition = { x: 0, y: 0 };
+const pickHelper = new PickHelper();
+clearPickPosition();
+
+
+
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - lastElapsedTime
@@ -352,11 +457,54 @@ const tick = () => {
     // Update controls
     controls.update()
 
+    pickHelper.pick(pickPosition, scene, camera);
+
     // Render
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
+window.requestAnimationFrame(tick)
+
+function getCanvasRelativePosition(event) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+        x: (event.clientX - rect.left) * canvas.width / rect.width,
+        y: (event.clientY - rect.top) * canvas.height / rect.height,
+    };
+}
+
+function setPickPosition(event) {
+    const pos = getCanvasRelativePosition(event);
+    pickPosition.x = (pos.x / canvas.width) * 2 - 1;
+    pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
+}
+
+function clearPickPosition() {
+    // unlike the mouse which always has a position
+    // if the user stops touching the screen we want
+    // to stop picking. For now we just pick a value
+    // unlikely to pick something
+    pickPosition.x = -100000;
+    pickPosition.y = -100000;
+}
+
+window.addEventListener('mousemove', setPickPosition);
+window.addEventListener('mouseout', clearPickPosition);
+window.addEventListener('mouseleave', clearPickPosition);
+
+window.addEventListener('touchstart', (event) => {
+    // prevent the window from scrolling
+    event.preventDefault();
+    setPickPosition(event.touches[0]);
+}, { passive: false });
+
+window.addEventListener('touchmove', (event) => {
+    setPickPosition(event.touches[0]);
+});
+
+window.addEventListener('touchend', clearPickPosition);
 
 tick()
+
